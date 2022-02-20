e47139ed3a334ec9fac37e9c94662582e9479b5c
Kevin Watke <Kevin@Susanas-iMac.local>
2022-02-19 18:30:30 -0600

    initial project setup

---
+++ WordGarden/WordGarden/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by Kevin Watke on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .link
+        
+    }
+
+
+}
+
